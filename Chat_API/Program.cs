d6f47dc3a57e91d0d930d99ac17b94cb3313e922
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-01-17 09:56:54 +0100

    access denied error solution

--- Chat_API/Program.cs
+++ Chat_API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,7 +13,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddCors(); 
+
+app.UseCors(builder => builder
+    .WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
