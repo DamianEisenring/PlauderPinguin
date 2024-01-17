5c36217bf720ef36180128a8a6cad330f3fe1f86
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-01-17 09:42:55 +0100

    Backend implemantation into Frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Login from './Components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PlauderPinguin</h1>
+        <Login/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
