f02d5d25108726b01b13373cb474a1eb86d2b9ad
xBl0oM <111044215+xBl0oM@users.noreply.github.com>
2024-01-24 11:26:49 +0100

    Add files via upload
    Added Frontent Components and Backend Controllers

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using Chat_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.X509.SigI;
+
+namespace Chat_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        public static Contact contact = new Contact();
+
+        [HttpGet("getContact")]
+        public IActionResult GetContact()
+        {
+            return Ok(contact);
+        }
+
+        [HttpPost("createContact")]
+        public IActionResult CreateContact([FromBody] Contact newContact)
+        {
+            if (newContact == null)
+            {
+                return BadRequest("Invalid contact data");
+            }
+
+            newContact.ContactId = contact.ContactId + 1;
+            contact = newContact;
+            return Ok(newContact);
+        }
+    }
+}
