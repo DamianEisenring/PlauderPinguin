968413ff17432f2313395b173ab07734f405de07
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-02-14 08:30:01 +0100

    Delete Models directory

--- Models/Contact.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chat_API.Models
-{
-    public class Contact
-    {
-        public string Contactname { get; set; } = string.Empty;
-
-        public int ContactId { get; set; }
-    }
-}
